85cc90a83c837e081586904ef17323d3df50a855
kakadante <danielmasila76@gmail.com>
2019-08-01 14:56:51 +0300

    Add project files.

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data.Entity;
+using GigHub.Models;
+using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)   /*Here we should ega load genre*/
+                .ToList();
+
+            return View(gigs);
+        }
+
+
+        [Authorize]
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            var followings = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(a => a.Followee)
+
+                .ToList();
+
+            return View(followings);
+        }
+
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Gig)
+                .Include(g => g.Artist)             /*Here we should ega load ARTIST*/
+                .Include(g => g.Genre)              /*Here we should ega load GENRE*/
+                .ToList();
+
+            var viewModel = new GigsViewModel()
+            {
+                UpcomingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Gig's I'm attending"
+            };
+
+            return View("Gigs", viewModel);
+        }
+
+
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                 Genres = _context.Genres.ToList(),   //initializes genres dropdown list
+                 Heading = "Add a Gig"
+            };
+
+
+            return View("GigForm", viewModel);
+        }
+
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel    /*Initialize all entries in the form*/
+            {
+                 Heading = "Edit a Gig",
+                 Id = gig.Id,
+                 Genres = _context.Genres.ToList(),
+                 Date = gig.DateTime.ToString("d MMM yyyy"),
+                 Time = gig.DateTime.ToString("HH:mm"),
+                 Genre = gig.GenreId,
+                 Venue = gig.Venue
+            };
+
+
+            return View("GigForm", viewModel);   /*We can use the same view that we used to capture a gig*/
+        }
+
+
+
+        //todo CREATE action - kwa GIGFORMVIEWMODEL
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),    //The user here enters DATE and TIME uniquely from GigFormViewModel
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs"); //Before the user was directed to ("index", "Home") index in HomeController
+                                                     //but we need to redirect them to mine after they Create a Gig
+        }
+
+
+
+
+        //todo UPDATE action - kwa GIGFORMVIEWMODEL
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs"); //Before the user was directed to ("index", "Home") index in HomeController
+                                                     //but we need to redirect them to mine after they Create a Gig
+        }
+    }
+}
+
+
+
+
+
+
+
+//todo I Changed the CREATE.cshtml to GIGFORM.cshtml since the VIEW was being used in both the CREATE and EDIT interfaces
+
+    //Actions mbili kabisa za mwisho tumeduplicate CREATE action ya MODELVIEW tukarename sacond one UPDATE
+
+    //Apo mahali tunarender FIELDS tukatoa ili tutumie DETAILS tayari stored kwa DB
+    //        var gig = _context.Gigs.Single(g => g.Id == viewModel.Id);
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,75 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Controllers;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+
+        public int Id { get; set; } /*we created this property for the ACTION property below to know if we are CREATING or UPDATING a gig*/
+                                    //Io public string  Action chini kabisa kwa ii Model--zoom pia @using (Html.BeginForm(Model.Action, "Gigs")) kwa GigForm
+
+
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+
+                //todo Izi ni za kutoa MAGIC STRINGS ueke expression in that ukichange code haita-break.
+
+                Expression<Func<GigsController, ActionResult>> update = (c => c.Update(this)); //For Update Action
+                                                               //unaeza eka NULL apa --- Update(NULL))
+
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this)); //For Update Action
+
+                //todo ADI APA
+
+                //? so afterwards
+                var action =  (Id != 0) ? update : create;
+
+                //todo we can execute this at RUN~TIME
+
+                return (action.Body as MethodCallExpression).Method.Name;
+
+                //alternatively this can be used
+                //var actionName = (action.Body as MethodCallExpression).Method.Name;
+
+
+
+                return (Id != 0) ? "Update" : "Create";
+            }
+        }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
