623406ef15980cac6433993b36ebc152d946ca97
kakadante <danielmasila76@gmail.com>
2019-08-14 17:19:17 +0300

    6. Refactoring Towards an Object-oriented Design From:        7. --- Fixing the Issue with Reverse Relationships
    ----------------------------------------------
    
    11. Using Factory Methods to Create Objects

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -25,8 +25,6 @@ namespace GigHub.Controllers.Api
                 .Include(g => g.Attendances.Select(a => a.Attendee))
                 .Single(g => g.Id == id && g.ArtistId == userId);
 
-
-
             //?--- KAMA GIG IKO CANCELED NA USER ANAITUMIA TENA nimeongeza ii CONDITION
                 if (gig.IsCanceled)
                 {
@@ -34,25 +32,7 @@ namespace GigHub.Controllers.Api
                 }
             //? --------------------------
 
-
-
-            gig.IsCanceled = true;
-
-            //? --- Gig Ikicanceliwa tutengeneze NOTIFICATION kwa USER
-            var notification = new Notification(NotificationType.GigCanceled, gig);
-
-//! After working on io Include apo juu, we wont need the below code, acha ni-comment tu.
-
-            //var attendees = _context.Attendances
-            //    .Where(a => a.GigId == gig.Id)
-            //    .Select(a => a.Attendee)
-            //    .ToList();
-
-            foreach (var attendee in gig.Attendances.Select(a => a.Attendee))
-            {
-                attendee.Notify(notification);
-            }
-            //? ------------------------------------------------
+            gig.Cancel();
 
             _context.SaveChanges();
 
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Dynamic;
+using System.Linq;
 using System.Net.Configuration;
+using System.Web.WebSockets;
 using Microsoft.Ajax.Utilities;
 
 namespace GigHub.Models
@@ -13,7 +15,7 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        public bool IsCanceled { get; set;}
+        public bool IsCanceled { get; private set;}
 
         public ApplicationUser Artist { get; set; }
 
@@ -36,6 +38,41 @@ namespace GigHub.Models
         public Gig()
         {
             Attendances = new Collection<Attendance>();
+        }
+
+        public void Cancel()
+        {
+            IsCanceled = true;
+
+            //? --- Gig Ikicanceliwa tutengeneze NOTIFICATION kwa USER
+            var notification = Notification.GigCanceled(this);
+
+            //! After working on io Include apo juu, we wont need the below code, acha ni-comment tu.
+
+            //var attendees = _context.Attendances
+            //    .Where(a => a.GigId == gig.Id)
+            //    .Select(a => a.Attendee)
+            //    .ToList();
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+            //? ------------------------------------------------
+        }
+
+        public void Modify(DateTime dateTime, string venue, byte genre)
+        { 
+            var notification = Notification.GigUpdated(this, DateTime, Venue);
+
+            Venue = venue;
+            DateTime = dateTime;
+            GenreId = genre;
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
         }
     }
 }
