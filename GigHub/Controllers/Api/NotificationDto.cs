780643d7ac2d778c9c17b8244e8a96a241aa1a52
kakadante <danielmasila76@gmail.com>
2019-08-16 07:06:58 +0300

    We created all these DTOs (Data Transfer Objects) and User AutoMapper from P.M.C to generate/reduce redundancy from code.

---
+++ GigHub/Controllers/Api/NotificationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GigHub.Models;
+
+namespace GigHub.Controllers.Api
+{
+    public class NotificationDto
+    {
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        public DateTime? OriginalDateTime { get; set; } //todo ? -- nullable DATETIME used when GIG is UPDATED
+        public string OriginalVenue { get; set; }
+        public GigDto Gig { get; set; }
+    }
+}
---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+
+            Mapper.CreateMap<ApplicationUser, UserDto>();
+            Mapper.CreateMap<Gig, GigDto>();
+            Mapper.CreateMap<Notification, NotificationDto>();
+
+
+
+            //! AFTER MAPPING WE CAN GET RID OF THIS
+
+                //return notifications.Select(n => new NotificationDto()
+                //{
+                //    DateTime = n.DateTime,
+                //    Gig = new GigDto
+                //    {
+                //        Artist = new UserDto
+                //        {
+                //            Id = n.Gig.Artist.Id,
+                //            Name = n.Gig.Artist.Name
+                //        },
+
+                //        DateTime = n.Gig.DateTime,
+                //        Id = n.Gig.Id,
+                //        IsCanceled = n.Gig.IsCanceled,
+                //        Venue = n.Gig.Venue
+                //    },
+
+                //    OriginalDateTime = n.OriginalDateTime,
+                //    OriginalVenue = n.OriginalVenue,
+                //    Type = n.Type
+                //});
+
+                return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+        }
+
+    }
+}
