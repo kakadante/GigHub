3b06235e024277015edc3d58a9ccfd455d489949
kaka_dante <danielmasila76@gmail.com>
2020-01-21 09:23:22 +0300

    ku-separate GigsContollers na atteandanceService na kuzieka kila moja folder yake
    app.js tunaiwacha pale pale incase we eed to add global variables
    
    enda u-add izo scripts mbili kwa BundleConfig.js
    
     Design ya kuunda individual js files uwork rahisi kw app ndogo, ikikua kubwa try:
    https://requirejs.org

---
+++ GigHub/Scripts/app/controllers/gigsController.js
@@ -0,0 +1,44 @@
+﻿var GigsController = function (attendanceService) {
+
+    var button;
+
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance);             /*js-toggle-attendance*/
+    };
+
+
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+
+        var gigId = button.attr("data-gig-id");
+
+        //todo 1.    $.post("/api/attendances", button.attr("data-gig-id"))
+        //?  cant work since the API action result is decorated with [FROM BODY]
+
+        //todo .    $.post("/api/attendances", { "":button.attr("data-gig-id")})
+        //?  This is the best alternative but the "" (the space) looks unprofessional
+
+
+        if (button.hasClass("btn-default"))
+            attendanceService.createAttendance(gigId, done, fail);
+        else
+            attendanceService.deleteAttendance(gigId, done, fail);
+    };
+
+
+    var done = function () {
+        var text = (button.text() == "WILL GO") ? "Going ?" : "WILL GO";
+
+        button.toggleClass("btn-warning").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function () {
+        alert("Something Failed");
+    };
+
+    return {
+        init: init
+    }
+
+}(AttendanceService);
---
+++ GigHub/Scripts/app/services/attendanceService.js
@@ -0,0 +1,29 @@
+﻿var AttendanceService = function () {
+
+    var createAttendance = function (gigId, done, fail) {
+        $.post("/api/attendances", { gigId: gigId }) //Making an AJAX call to our API
+            //?  This is the best - gigId instead of the space. (cleaner way)
+            //todo  TO WRAP THE [FROM BODY]GIGID PARAMETER - IN attendancecontroller api, LETS create another class above
+
+            //SO WE HAVE e.target IN THE LINE ABOVE AND BELOW. Lets make a VARIABLE to store it
+            .done(done)
+            .fail(fail);
+    };
+
+
+    var deleteAttendance = function (gigId, done, fail) {
+        $.ajax({
+            url: "/api/attendances/" + gigId,
+            method: "DELETE"
+        })
+
+
+            .done(done)
+            .fail(fail);
+    };
+
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    }
+}();
